cc574ec06af5f073a50bcea298f33b11fdbc0d29
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 05:45:57 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-          name: "Benolvio",
+          name: "Benvolio",
           age: "17",
           attitude: "worried"
         },
